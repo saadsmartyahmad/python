010f3fbdf187bbece774b880a669efad848e1661
saadsmartyahmad <79416567+saadsmartyahmad@users.noreply.github.com>
2022-06-21 09:28:57 +0530

    Add files via upload

---
+++ python project code/StringCase.py
@@ -0,0 +1,30 @@
+print(" string case in python")
+
+a="software"
+
+print(a.upper())
+
+print("string built in method in python")
+
+a="SOFTWARE"
+print(a.lower())
+
+print("print format in python there is two way to print format in python first is simple and second is by using format function")
+
+a="amit"
+b=12
+c=23.3
+
+print("name is",a,"age is",b,"salary is",c)
+
+print("by using format function")
+
+
+name="python"
+
+age=1888
+
+salary=90000.0
+
+print("name is {} age is {} salary is {}".format(name,age,salary))
+
